46ef921a9880583fe1e41364c474092ed1a812cc
KingsonKai <562419972@qq.com>
2023-06-02 17:25:49 +0800

    enhancement: Optimize deployment scripts and support local deployment

--- chaosmeta-inject-operator/build/deploy/undeploy.sh
+++ chaosmeta-inject-operator/build/deploy/undeploy.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
 set -e
+
+NOW_DIR=`cd $(dirname $0); pwd`
 VERSION=$1
-ROOT_PATH=$(dirname $(readlink -f $0))/chaosmeta_build
-echo "${ROOT_PATH}"
-mkdir -p "${ROOT_PATH}"/yamls
-curl -o "${ROOT_PATH}"/yamls/chaosmeta.yaml https://raw.githubusercontent.com/traas-stack/chaosmeta/"${VERSION}"/chaosmeta-inject-operator/build/yamls/chaosmeta.yaml
+if [ -z "$VERSION" ]; then
+    echo "version is empty"
+    exit 1
+fi
+echo "version: ${VERSION}"
+
+function downloadfile() {
+    url=$1
+    filename=$2
+    echo "download from: ${url}"
+    if [ ! -e "${filename}" ]; then
+        curl -o "${filename}" "${url}"
+    fi
+    echo "download success: ${filename}"
+}
+
+ROOT_PATH=${NOW_DIR}/../
+if [ "${VERSION}" != "local" ]; then
+    ROOT_PATH=${NOW_DIR}/chaosmeta_build
+    echo "${ROOT_PATH}"
+    mkdir -p "${ROOT_PATH}"/yamls
+    filename="${ROOT_PATH}"/yamls/chaosmeta.yaml
+
+    if [ ! -e "${filename}" ]; then
+        downloadfile https://raw.githubusercontent.com/traas-stack/chaosmeta/"${VERSION}"/chaosmeta-inject-operator/build/yamls/chaosmeta.yaml "${filename}"
+    fi
+fi
+
 kubectl delete -f "${ROOT_PATH}"/yamls/chaosmeta.yaml
