0729e6427a9887b624ae310832c4d9eb793f02a3
KingsonKai <562419972@qq.com>
2023-08-29 10:16:48 +0800

    fix: deploy for platform mysql

--- chaosmeta-deploy/deploy.sh
+++ chaosmeta-deploy/deploy.sh
@@ -20,27 +20,29 @@ BASE_DIR=$(
 
 # param check
 OP=$1
-COMPONENT=$2
-NAMESPACE=$3
+NAMESPACE=$2
+COMPONENT_LIST=()
+for ((i = 3; i <= $#; i++)); do
+  COMPONENT_LIST+=("${!i}")
+done
 
-if [[ -z "$OP" || -z "$COMPONENT" ]]; then
-  echo "sh deploy.sh [operation] [component] [namespace]\n  operation support: ${OP_INSTALL},${OP_UNINSTALL}\n  component support: ${COMPONENT_ALL},${COMPONENT_PLATFORM},${COMPONENT_WORKFLOW},${COMPONENT_INJECT},${COMPONENT_MEASURE},${COMPONENT_FLOW},${COMPONENT_DAEMON}"
+if [ "$OP" == "-h" ] || [ "$OP" == "--help" ] || [ -z "$OP" ] || [ ${#COMPONENT_LIST[@]} -eq 0 ]; then
+  echo "format: sh deploy.sh [operation] [namespace] [component list]"
+  echo "operation support: ${OP_INSTALL},${OP_UNINSTALL}"
+  echo "component support: ${COMPONENT_ALL} ${COMPONENT_PLATFORM} ${COMPONENT_WORKFLOW} ${COMPONENT_INJECT} ${COMPONENT_MEASURE} ${COMPONENT_FLOW} ${COMPONENT_DAEMON}"
+  echo "example: sh deploy.sh install default ${COMPONENT_PLATFORM} ${COMPONENT_WORKFLOW} ${COMPONENT_INJECT} ${COMPONENT_DAEMON}"
   exit 1
 fi
 
 if [[ "$OP" != "$OP_INSTALL" && "$OP" != "$OP_UNINSTALL" ]]; then
-  echo "not support operation: $OP\n  operation support: ${OP_INSTALL},${OP_UNINSTALL}"
-  exit 1
-fi
-
-if [[ "$COMPONENT" != "$COMPONENT_WORKFLOW" && "$COMPONENT" != "$COMPONENT_DAEMON" && "$COMPONENT" != "$COMPONENT_ALL" && "$COMPONENT" != "$COMPONENT_PLATFORM" && "$COMPONENT" != "$COMPONENT_INJECT" && "$COMPONENT" != "$COMPONENT_MEASURE" && "$COMPONENT" != "$COMPONENT_FLOW" ]]; then
-  echo "not support component: $COMPONENT\n  component support: ${COMPONENT_ALL},${COMPONENT_PLATFORM},${COMPONENT_WORKFLOW},${COMPONENT_INJECT},${COMPONENT_MEASURE},${COMPONENT_FLOW},${COMPONENT_DAEMON}"
+  echo "not support operation: $OP"
+  echo "operation support: ${OP_INSTALL},${OP_UNINSTALL}"
   exit 1
 fi
 
 if [[ -z "$NAMESPACE" ]]; then
-  echo "not provide namespace, set to \"default\""
-  NAMESPACE="default"
+  echo "please provide namespace"
+  exit 1
 fi
 
 # check namespace exist
@@ -48,6 +50,20 @@ if [[ $(kubectl get ns $NAMESPACE | wc -l) -eq 0 ]]; then
   echo "namespace[$NAMESPACE] not exist, please create ns first"
   exit 2
 fi
+
+for COMPONENT in "${COMPONENT_LIST[@]}"; do
+  if [ ${#COMPONENT_LIST[@]} -ne 1 ] && [ "$COMPONENT" == "$COMPONENT_ALL" ]; then
+    echo "component[$COMPONENT_ALL] should not in list with other components"
+    exit 3
+  fi
+
+  if [[ "$COMPONENT" != "$COMPONENT_WORKFLOW" && "$COMPONENT" != "$COMPONENT_DAEMON" && "$COMPONENT" != "$COMPONENT_ALL" && "$COMPONENT" != "$COMPONENT_PLATFORM" && "$COMPONENT" != "$COMPONENT_INJECT" && "$COMPONENT" != "$COMPONENT_MEASURE" && "$COMPONENT" != "$COMPONENT_FLOW" ]]; then
+    echo "not support component: $COMPONENT"
+    echo "component support: ${COMPONENT_ALL} ${COMPONENT_PLATFORM} ${COMPONENT_WORKFLOW} ${COMPONENT_INJECT} ${COMPONENT_MEASURE} ${COMPONENT_FLOW} ${COMPONENT_DAEMON}"
+    echo "example: sh deploy.sh install default ${COMPONENT_PLATFORM} ${COMPONENT_WORKFLOW} ${COMPONENT_INJECT} ${COMPONENT_DAEMON}"
+    exit 1
+  fi
+done
 
 function installComponent() {
   sed "s/${NAMESPACE_REPLACE}/$2/g" ${BASE_DIR}/templates/chaosmeta-$1-template.yaml >${BASE_DIR}/yamls/chaosmeta-$1.yaml
@@ -65,13 +81,15 @@ function installComponent() {
 }
 
 # execute install
-if [[ "$COMPONENT" == "$COMPONENT_ALL" ]]; then
-  installComponent $COMPONENT_PLATFORM $NAMESPACE
-  installComponent $COMPONENT_WORKFLOW $NAMESPACE
-  installComponent $COMPONENT_INJECT $NAMESPACE
-  installComponent $COMPONENT_DAEMON $NAMESPACE
-  installComponent $COMPONENT_MEASURE $NAMESPACE
-  installComponent $COMPONENT_FLOW $NAMESPACE
-else
-  installComponent $COMPONENT $NAMESPACE
-fi
+for COMPONENT in "${COMPONENT_LIST[@]}"; do
+  if [[ "$COMPONENT" == "$COMPONENT_ALL" ]]; then
+    installComponent $COMPONENT_PLATFORM $NAMESPACE
+    installComponent $COMPONENT_WORKFLOW $NAMESPACE
+    installComponent $COMPONENT_INJECT $NAMESPACE
+    installComponent $COMPONENT_DAEMON $NAMESPACE
+    installComponent $COMPONENT_MEASURE $NAMESPACE
+    installComponent $COMPONENT_FLOW $NAMESPACE
+  else
+    installComponent $COMPONENT $NAMESPACE
+  fi
+done
