d5a165faa6c2ba7146bf486279758eca28dc68c4
zwk1091 <wenkun97@163.com>
2023-12-22 17:37:59 +0800

    fix: update crd, simplify some code

--- chaosmeta-inject-operator/pkg/selector/selector_test.go
+++ chaosmeta-inject-operator/pkg/selector/selector_test.go
@@ -97,11 +97,12 @@ func TestGetTargetContainer(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotR, gotId, gotName, err := GetTargetContainer(tt.args.containerName, tt.args.status)
-			if (err != nil) != tt.wantErr {
+			containers, err := GetTargetContainers(tt.args.containerName, tt.args.status)
+			if (err != nil) != tt.wantErr || len(containers) == 0 {
 				t.Errorf("GetTargetContainer() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+			gotR, gotId, gotName := containers[0].ContainerRuntime, containers[0].ContainerId, containers[0].ContainerName
 			if gotR != tt.wantR {
 				t.Errorf("GetTargetContainer() gotR = %v, want %v", gotR, tt.wantR)
 			}
